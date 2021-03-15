2ffcc253ea126a14a33e227a45cf73891b9bfbd0
Sridhar Iyer <iyer.a.sri@gmail.com>
2021-03-15 09:40:34 -0500

    gunicorn intermittently fails to fetch yt_data dict vales

--- README.md
+++ README.md
@@ -4,17 +4,13 @@
 
 1. `git clone <this repo>`
 1. Create a virtual environment.
-1. `pip install -r requirements.txt`
+1. Have `ffmpeg` installed.
+1. If you want to host this on a Raspberry Pi, then [follow these instructions](https://www.techcoil.com/blog/how-to-setup-python-imaging-library-pillow-on-raspbian-stretch-lite-for-processing-images-on-your-raspberry-pi/) to install the `pillow` package dependencies.
+1. `pip install -U pip setuptools bootstrap-flask requests eyed3 flask-wtf pillow youtube-dl`
 1. `export FLASK_APP=app.py`
 1. `flask run`
-1. Launch `http://127.0.0.1:5000/` in a browser.
+1. Launch `http://127.0.0.1:5000/`
 
 ### User Guide
 
-1. Follow the screenshots below.
-
-![](images/S1.PNG)
-
-![](images/S2.PNG)
-
-![](images/S3.PNG)
+1. Follow the screenshots in the `images/` folder
--- app.py
+++ app.py
@@ -25,12 +25,7 @@ app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'darkly'  # uncomment this line to te
 bootstrap = Bootstrap(app)
 csrf = CSRFProtect(app)
 
-global encoded_img_data
-global byte_img
-global yt_info
-global url
-global output_filepath_mp3
-global filename
+yt_data = {}
 
 home = expanduser("~")
 MUSIC_DIR = os.path.join(home, 'Music')
@@ -45,31 +40,30 @@ def convert_sec_to_hms(seconds):
 
 
 def download_and_convert_to_mp3(form):
-    base, ext = os.path.splitext(output_filepath_mp3)
+    base, ext = os.path.splitext(yt_data.get('output_filepath_mp3'))
     output_filepath_ytdl_ext = base + '.%(ext)s'
     subprocess.run(
         ['youtube-dl', '-f', 'bestaudio', '--restrict-filenames', '--extract-audio', '--audio-format', 'mp3', '-o',
-         output_filepath_ytdl_ext, url])
+         output_filepath_ytdl_ext, yt_data.get('url')])
 
-    audio_file = eyed3.load(output_filepath_mp3)
+    audio_file = eyed3.load(yt_data.get('output_filepath_mp3'))
     audio_file.initTag()
     audio_file.tag.artist = form.artist.data
     audio_file.tag.genre = form.genre.data
     audio_file.tag.album_artist = form.album_artist.data
     audio_file.tag.album = form.album.data
     audio_file.tag.composer = form.composer.data
-    audio_file.tag.images.set(3, byte_img.getvalue(), 'image/jpeg')
+    audio_file.tag.images.set(3, yt_data.get('byte_img').getvalue(), 'image/jpeg')
     audio_file.tag.save()
 
 
 def encoded_img():
-    response = requests.get(yt_info['thumbnail'])
+    response = requests.get(yt_data.get('yt_info').get('thumbnail'))
     img = Image.open(io.BytesIO(response.content))
-    global byte_img
     byte_img = io.BytesIO()
     img.save(byte_img, "JPEG")
-    global encoded_img_data
-    encoded_img_data = base64.b64encode(byte_img.getvalue())
+    yt_data['byte_img'] = byte_img
+    yt_data['encoded_img_data'] = base64.b64encode(byte_img.getvalue())
 
 
 genre_values = ('Alternative', 'Bhajans', 'BWW', 'Carnatic Fusion',
@@ -93,21 +87,15 @@ class YTDLForm(FlaskForm):
     download = SubmitField()
 
 
-@app.route('/success')
-def success(song='Ram Bhajan'):
-    flash(f'Song {song} Successfully Downloaded as MP3!', 'success')
-    return render_template('success.html', song=song)
-
-
 @app.route('/ytform', methods=['GET', 'POST'])
 def yt_form():
     form = YTDLForm()
     if form.validate_on_submit():
         download_and_convert_to_mp3(form=form)
-        flash(f'Song {filename[:20]}...mp3 successfully downloaded', 'success')
+        flash(f'Song {yt_data.get("filename")[:20]}...mp3 successfully downloaded', 'success')
         return redirect(url_for('home'))
 
-    return render_template('yt_form.html', form=form, img_data=encoded_img_data.decode('utf-8'))
+    return render_template('yt_form.html', form=form, img_data=yt_data.get('encoded_img_data').decode('utf-8'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -115,23 +103,26 @@ def yt_form():
 def home():
     form = YTGetInfoForm()
     if form.validate_on_submit():
-        global yt_info
-        global url
-        url = form.url.data
+        yt_data['url'] = form.url.data
         with youtube_dl.YoutubeDL() as ydl:
-            yt_info = ydl.extract_info(url, download=False)
+            yt_data['yt_info'] = ydl.extract_info(form.url.data, download=False)
         # Geting output file path and checking it already exists
-        global filename
-        filename = re.sub(r'[^\x00-\x7f]', r'', yt_info["title"])  # Removing all non-ascii
+        filename = re.sub(r'[^\x00-\x7f]', r'', yt_data['yt_info']["title"])  # Removing all non-ascii
         filename = re.sub('[^0-9a-zA-Z]+', '_', filename)  # Replacing all non-alpha-numeric with '_'
-        global output_filepath_mp3
-        output_filepath_mp3 = os.path.join(MUSIC_DIR, filename + '.mp3')
+        yt_data['filename'] = filename
+        yt_data['output_filepath_mp3'] = os.path.join(MUSIC_DIR, filename + '.mp3')
 
-        if not os.path.exists(output_filepath_mp3):
+        print(f"\n{yt_data['output_filepath_mp3']}\n")
+
+        if not os.path.exists(yt_data['output_filepath_mp3']):
             encoded_img()
             return redirect(url_for('yt_form'))
         else:
-            flash(f'Song {filename[:20]}...mp3 already exists', 'warning')
+            flash(f'Song {yt_data.get("filename")[:20]}...mp3 already exists', 'warning')
             return redirect(url_for('home'))
 
     return render_template('yturl.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4400)
